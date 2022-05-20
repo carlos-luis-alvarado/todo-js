29fbe9ade28ad587202768cff8cae1b2380e3d36
Carlos-Alvarado <alvaradocarlosluis97@gmail.com>
2022-05-20 18:52:40 -0300

    primer commit

---
+++ src/classes/todo.class.js
@@ -0,0 +1,21 @@
+//usamos export porque esta clases se usara en otro archivo
+export class Todo{
+
+    static fromJson({id,tarea,completado,creado}){
+        const tempTodo = new Todo(tarea);
+        tempTodo.id = id;
+        tempTodo.completado=completado;
+        tempTodo.creado=creado;
+        return tempTodo;
+    }
+    constructor(tarea){
+        this.tarea = tarea;
+        this.id = new Date().getTime();//permite genrar como un id
+        this.completado = false;
+        this.creado = new Date();
+    }
+
+    imprimirClase(){
+        console.log(`${this.tarea} - ${this.id}`)
+    }
+}
